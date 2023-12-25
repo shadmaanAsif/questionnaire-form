6d7d08ad14e109e1bb937ddb4ba3c1403dbc80c0
Mohammad Shadmaan Asif <89839998+shadmaanAsif@users.noreply.github.com>
2023-12-25 17:37:23 +0400

    fix: initial commit (#1)
    Co-authored-by: Mohammad Shadmaan Asif <shadmaan.asif@DXBTAJFE-MB002.local>

---
+++ pages/formId/[...page].tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { useForm } from 'react-hook-form';
+
+import './styles.css';
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  }; // your form submit function which will invoke after successful validation
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => {
+        // Check if the request was successful (status code 2xx)
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        // Parse the JSON response
+        return response.json();
+      })
+      .then((data) => {
+        // Handle the data
+        console.log('Data:', data);
+        setData(data);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error('Fetch error:', error);
+      });
+  }, []);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue="test" {...register('example')} />
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register('exampleRequired', { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <p>This field is required</p>}
+
+      <input type="submit" />
+    </form>
+  );
+}
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
+export default App;
